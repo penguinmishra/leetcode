b8028344dac416a25be72655ac80ca278ddebaa9
penguinmishra@gmail.com <penguinmishra@gmail.com>
2022-10-21 08:07:10 +0530

    Adds another solution to contains duplicate II

--- src/com/code/leetcode/ContainsDuplicateII.java
+++ src/com/code/leetcode/ContainsDuplicateII.java
@@ -1,6 +1,8 @@
 package com.code.leetcode;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -14,11 +16,20 @@ public static void main(String[] args) {
 }
 
 public boolean containsNearbyDuplicate(int[] nums, int k) {
-    Set<Integer> set = new HashSet<>();
+	Set<Integer> set = new HashSet<>();
+	for (int i = 0; i < nums.length; i++) {
+		if (i > k) set.remove(nums[i - k - 1]);
+		boolean present = !set.add(nums[i]);
+		if (present) return true;
+	}
+	return false;
+}
+
+public boolean containsNearbyDuplicate2(int[] nums, int k) {
+    Map<Integer, Integer> map = new HashMap<>();
     for(int i = 0; i < nums.length; i++){
-        if(i > k) set.remove(nums[i - k - 1]);
-        boolean present = !set.add(nums[i]);
-        if(present) return true;
+        if(map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) return true;
+        map.put(nums[i], i);
     }
     return false;
 }
